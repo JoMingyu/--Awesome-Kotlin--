7c5192970ed2ed0fbf573c506c1c71ee24631e62
JoMingyu <city7310@naver.com>
2017-06-23 12:54:42 +0900

    Annotation class

---
+++ 28. Annotation class/src/Main.kt
@@ -0,0 +1,13 @@
+annotation class Hello
+
+@Target(AnnotationTarget.TYPE)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class Good
+
+annotation class Nice(
+        val message: String
+)
+
+@Hello @Nice("okok") fun main(args: Array<String>) {
+    
+}
