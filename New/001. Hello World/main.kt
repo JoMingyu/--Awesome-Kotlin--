8a1f26f965fa8ecc02b149cd58356a5ea8862065
JoMingyu <city7310@naver.com>
2018-04-24 08:40:06 +0900

    New - 001

---
+++ New/001. Hello World/main.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+    // 메인 함수 정의, Class-less하게 함수를 정의할 수 있으며
+    // 이는 Kotlin의 entry point
+    // Kotlin의 확장자는 .kt
+    
+    // 주석은 위처럼 slash(/) 기호 2개를 연달아 표기하여 표현할 수 있으며
+    /*
+    이처럼 asterisk(*) 기호를 통해
+    여러 줄에 걸친 주석을 표현할 수 있다
+     */
+
+    println("Hello World")
+    // println 함수로 출력과 함께 개행
+}
