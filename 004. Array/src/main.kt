734d194d39f02cd89c603188b361198fd5690cca
JoMingyu <city7310@naver.com>
2018-05-24 19:17:02 +0900

    다 없애버려야지

--- 004. Array/src/main.kt
+++
@@ -1,9 +0,0 @@
-fun main(args: Array<String>) {
-    // IntArray, DoubleArray, LongArray 등 데이터 타입마다 Array 데이터형이 있으며 제네릭 형태도 제공됨
-    val arr = arrayOf(1, 2, 3, 4)
-    // Array<T>를 반환
-
-    arr.iterator().forEach { println(it) }
-    println(arr[2])
-    // 코틀린의 array는 컬렉션에 가깝지만 연산자 오버로딩 지원 덕분에 인덱싱도 가능함
-}
--- 005. Package/src/some/pack.kt
+++
@@ -1,6 +0,0 @@
-package some
-// 패키지 선언
-
-fun f(): String {
-    return "hello"
-}
