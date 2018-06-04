8fd0da0e241e4137d85411390dc532cb87815c32
JoMingyu <city7310@naver.com>
2018-06-04 13:53:32 +0900

    006

---
+++ 006. Methods of String/main.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>) {
+    // https://kotlinlang.org/api/latest/jvm/stdlib/kotlin/-string/index.html
+    
+    println("PlanB".contains("an"))
+    // true
+    
+    println("PlanB".reversed())
+    // BnalP
+
+    println("PlanB".indexOf("B"))
+    // 4
+
+    println("".isEmpty())
+    // true
+
+    println(" ".isNotEmpty())
+    // true
+}
